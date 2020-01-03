2dce206929b0e0c1e49945706f5bf26eef92d1fb
Harrison Thomas <hthomas146@utexas.edu>
2020-01-03 16:12:27 -0600

    Hardcoding the forecast using many functions.

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ $("#day4").text(day4);
 $("#day5").text(day5);
 $("#day6").text(day6);
 
+var day2Temp = [];
+var day2Hum = 0;
+
 // When the submit button is clicked
 // A query is sent to the OWM api to grab information about the current weather
 // An object is returned    
@@ -23,6 +26,40 @@ $("button").on("click", function () {
     event.preventDefault();
     var citySubmit = $("#city-search").val();
     console.log(citySubmit);
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySubmit + "&appid=83ec25aafa25787879adb49e8ad70c00&units=imperial";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(queryURL);
+        console.log(response);
+
+        function day2InfoFind() {
+            for (i = 1; i < 9; i++) {
+                day2Temp.push(Math.floor(response.list[i].main.temp_max));
+                day2Hum = day2Hum + response.list[i].main.humidity;
+            }
+            day2Temp = Math.max.apply(Math, day2Temp);
+            console.log(day2Temp);
+            day2Hum = (day2Hum / 8);
+            console.log(day2Hum);
+            document.querySelector("#day2-temp").innerHTML = day2Temp;
+            document.querySelector("#day2-hum").innerHTML = day2Hum;
+
+        }
+
+
+        day2InfoFind();
+
+
+        //write a function to calculate the temperature for the current day
+        //for response[0] - response[7]
+
+    })
+
+
 })
 
 // When the submit button is clicked
@@ -33,3 +70,20 @@ $("button").on("click", function () {
 // When the submit button is clicked
 // The name of the city is saved to LocalStorage
 // The last ten results searched are displayed below the search bar
+
+
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function (response) {
+//     console.log(queryURL);
+//     var results = response;
+//     var temperature = (((response.main.temp) - 273.15) * 1.8) + 32;
+//     console.log(results);
+//     console.log(temperature);
+//     $("#city-name-current").text(citySubmit);
+//     $("#temperature-main").text(Math.floor(temperature));
+//     $("#humidity-main").text("Humidity: " + response.main.humidity);
+//     $("#windspeed-main").text("Wind-speed: " + results.wind.speed + " mph");
+
+// })
